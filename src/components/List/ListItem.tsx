99e05029eb140c2cb2516e64019ce6a63b8b61f1
Blazh <iblazevi@gmail.com>
2019-07-01 18:25:48 +0200

    Update ListItem.tsx

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Item } from '../../models/item';
 import './Bars.css';
+import { Swipeable } from 'react-swipeable'
 
 interface ListItemProps {
     item: Item;
@@ -18,69 +19,13 @@ class ListItem extends React.Component<any> {
 
     listElement: any;
 
-    // Drag & Drop
-    dragStartX = 0
-    left = 0
-    dragged = false
-
     constructor(props: any) {
         super(props);
 
-        this.onDragStartTouch = this.onDragStartTouch.bind(this);
         this.onPlay = this.onPlay.bind(this);
         this.onDelete = this.onDelete.bind(this);
-
-
-        this.onTouchMove = this.onTouchMove.bind(this);
-        this.onDragStartTouch = this.onDragStartTouch.bind(this);
-        this.onDragEndTouch = this.onDragEndTouch.bind(this);
-        this.onDragEnd = this.onDragEnd.bind(this);
         this.onSwiped= this.onSwiped.bind(this);
-
         this.listElement = {}
-    }
-
-    componentDidMount() {
-        window.addEventListener("touchend", this.onDragEndTouch);
-    }
-
-    onDragStartTouch(evt: any) {
-        const touch = evt.targetTouches[0];
-        this.onDragStart(touch.clientX);
-        window.addEventListener("touchmove", this.onTouchMove);
-    }
-
-    onDragEndTouch(evt: any) {
-        window.removeEventListener("touchmove", this.onTouchMove);
-        this.onDragEnd();
-    }
-
-    onDragStart(clientX: any) {
-        this.dragged = true;
-        this.dragStartX = clientX;
-    }
-
-    onDragEnd() {
-        if (this.dragged) {
-          this.dragged = false;
-
-          const threshold = this.props.threshold || 0.3;
-      
-          if (this.left < this.listElement.offsetWidth * threshold * -1) {
-            this.left = -this.listElement.offsetWidth * 2;
-            this.onSwiped();
-          } else {
-            this.left = 0;
-          }
-        }
-    }
-
-    onTouchMove(evt: any) {
-        const touch = evt.targetTouches[0];
-        const left = touch.clientX - this.dragStartX;
-        if (left < 0) {
-          this.left = left;
-        }
     }
 
     onSwiped() {
@@ -102,24 +47,26 @@ class ListItem extends React.Component<any> {
 
     render() {
         return (
-            <li ref={div => (this.listElement = div)}
-                className={this.state.deleted ? 'deleted' : ''}
-                onTouchStart={this.onDragStartTouch}
-                onClick={this.onPlay}>
-                <div className={["item-container", this.state.swiped ? 'swiped' : ''].join(' ')}>
-                    <span className={this.props.selected ? 'hidden' : ''}>{this.props.index + 1}. </span>
-                    <div id="bars" className={this.props.selected ? 'selected' : ''}>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
-                        <div className="bar"></div>
+            <Swipeable onSwipedLeft={this.onSwiped} trackMouse={true}>
+                <li ref={div => (this.listElement = div )}
+                    className={this.state.deleted ? 'deleted' : ''}
+                    onClick={this.onPlay}
+                    >
+                    <div className={["item-container", this.state.swiped ? 'swiped' : ''].join(' ')}>
+                        <span className={this.props.selected ? 'hidden' : ''}>{this.props.index + 1}. </span>
+                        <div id="bars" className={this.props.selected ? 'selected' : ''}>
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                        </div>
+                        {this.props.item.title}
+                        <div onClick={this.onDelete} className={["delete", this.state.swiped ? 'swiped' : ''].join(' ')}>
+                            Delete
+                        </div>
                     </div>
-                    {this.props.item.title}
-                    <div onClick={this.onDelete} className={["delete", this.state.swiped ? 'swiped' : ''].join(' ')}>
-                        Delete
-                    </div>
-                </div>
-            </li>
+                </li>
+            </Swipeable>
         )
     }
 }
